71c0f477454bf74cc9319fe2f99cecbd9a57fbdf
Vini Antunes <vinicius.aoliveira@yduqs.com.br>
2025-10-04 22:16:05 -0300

    Reaponta chamada das classes Item() e Pessoa()

--- classes/item.py
+++ classes/item.py
@@ -2,25 +2,24 @@
 class Item(object):
 	def __init__(self):
 		self.data = {
-				'ovo' : 0,
-				'leite' : 0,
-				'banana' : 0,
-				'trigo' : 0,
-				'margarina' : 0,  # <- Uma colher = 0.08
-				'acucar' : 0,
-				'fermento' : 0,
-				'oleo' : 0,
-				'sal' : 0,
-				'bolo' : 0,
-				'pao' : 0,
-				'ovo_frito' : 0,  # unidade
-				'panqueca' : 0,  # unidade
-				}
+            'ovo' : 0,
+            'leite' : 0,
+            'banana' : 0,
+            'trigo' : 0,
+            'margarina' : 0,  # <- Uma colher = 0.08
+            'acucar' : 0,
+            'fermento' : 0,
+            'oleo' : 0,
+            'sal' : 0,
+            'bolo' : 0,
+            'pao' : 0,
+            'ovo_frito' : 0,  # unidade
+            'panqueca' : 0,  # unidade
+        }
 
 	def ganhar_item(self, **kwargs):
 		for kw in kwargs:
 			self.data[kw] += kwargs[kw]
-
 
 	def perder_item(self, **kwargs):
 		for kw in kwargs:
@@ -31,4 +30,3 @@ class Item(object):
 	def debug(self):
 		for i in self.data:
 			self.data[i] = 500
-
